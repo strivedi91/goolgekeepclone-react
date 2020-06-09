45f9e1e5b0a8296023a92e12147ffb68cf64249d
Sachin Trivedi <sachindtrivedi@gmail.com>
2020-06-09 23:22:48 +0530

    added delete notes feature

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,23 +1,46 @@
-import React, { Component } from "react";
+import React, { useContext, useState } from "react";
+import { NotesContext } from "../context/NotesContext";
 
-export default class AddNote extends Component {
-  render() {
-    return (
-      <div className="grid grid-cols-6 ">
-        <div className="col-start-2 col-span-4 shadow appearance-none border rounded text-center py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-          <input
-            className="mb-4 w-full outline-none font-bold"
-            id="title"
-            type="text"
-            placeholder="Title"
-          />
-          <textarea
-            className="w-full outline-none"
-            id="description"
-            placeholder="Take a note"
-          />
-        </div>
+const AddNote = () => {
+  const [notes, setNotes] = useContext(NotesContext);
+
+  const addNewNote = () => {
+    if (title || description) {
+      setNotes([{ title: title, description: description }, ...notes]);
+      setTitle("");
+      setDescription("");
+    }
+  };
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="grid grid-cols-6 ">
+      <div
+        className="col-start-2 col-span-4 shadow appearance-none border 
+      rounded text-center py-2 px-3 text-gray-700 leading-tight 
+      focus:outline-none focus:shadow-outline"
+        onBlur={addNewNote}
+      >
+        <input
+          className="mb-4 w-full outline-none font-bold"
+          id="title"
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          className="w-full outline-none"
+          id="description"
+          placeholder="Take a note"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default AddNote;
--- src/components/DeleteNote.js
+++ src/components/DeleteNote.js
@@ -1,8 +1,18 @@
 import React, { useContext } from "react";
 import { SelectedNotesContext } from "../context/SelectedNotesContext";
+import { NotesContext } from "../context/NotesContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function DeleteNote() {
-  const [selectedNotes] = useContext(SelectedNotesContext);
+  const [selectedNotes, setSelectedNotes] = useContext(SelectedNotesContext);
+  const [notes, setNotes] = useContext(NotesContext);
+
+  const deleteNotes = () => {
+    let filteredNotes = notes.filter((x) => !selectedNotes.includes(x._id));
+    setNotes([...filteredNotes]);
+    setSelectedNotes([]);
+  };
 
   return (
     <div>
@@ -12,7 +22,9 @@ export default function DeleteNote() {
           role="alert"
         >
           <strong className="font-bold">{selectedNotes.length} selcted</strong>
-          <span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
+          <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <FontAwesomeIcon icon={faTrash} onClick={deleteNotes} />
+          </span>
         </div>
       )}
     </div>
